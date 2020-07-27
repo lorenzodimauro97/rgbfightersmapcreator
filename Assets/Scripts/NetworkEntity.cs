e26cf0db02d18970cccd2cda322f9763cf5f9669
Scippo97 <lorenzo.dimauro97@gmail.com>
2020-07-27 23:04:34 +0200

    first commit

---
+++ Assets/Scripts/NetworkEntity.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class NetworkEntity : MonoBehaviour
+{
+    public enum Entity{ Ammo, Gun, Movable }
+
+    public Entity entityType;
+    public string gunId;
+    public string entityId;
+}
